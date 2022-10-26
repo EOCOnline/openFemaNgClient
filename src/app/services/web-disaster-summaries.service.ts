ead79e11dcf6635f5f117fe4db1bdb83f669aaf7
JCornelison <gitKraken@VashonDesign.com>
2022-10-25 18:56:53 -0700

    initial code scalfolding...

---
+++ src/app/services/web-disaster-summaries.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+
+// https://www.fema.gov/openfema-data-page/disaster-declarations-summaries-v2
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebDisasterSummariesService {
+
+  constructor() { }
+}
