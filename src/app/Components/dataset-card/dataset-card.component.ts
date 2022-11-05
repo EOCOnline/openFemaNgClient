25f99564d53049964534cb4b16dac29bc8fefc01
JCornelison <gitKraken@VashonDesign.com>
2022-11-05 12:14:04 -0700

    moving from standalone component

--- src/app/Components/dataset-card/dataset-card.component.ts
+++ src/app/Components/dataset-card/dataset-card.component.ts
@@ -2,27 +2,23 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DisasterDeclarationsSummaryType, WebDisasterSummariesService } from 'src/app/services';
 import { RouterModule } from '@angular/router';
-//import { DatasetCardComponent } from './dataset-card.component';
-//import { DatasetViewerComponent } from '../../app.component';
+import { DatasetViewerComponent} from './../';
 
+//standalone: true,
+//imports: [CommonModule, RouterModule],
 @Component({
   selector: 'app-dataset-card',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
   templateUrl: './dataset-card.component.html',
   styleUrls: ['./dataset-card.component.scss']
 })
 export class DatasetCardComponent implements OnInit {
-/*
-  @  Input() disaster : DisasterDeclarationsSummaryType | null = null
-  @   Input() index : Number | null = null
-*/
-  constructor(readonly webDisasterSummariesService: WebDisasterSummariesService) {
-    /*
+  @Input() disaster : DisasterDeclarationsSummaryType | null = null
+  @Input() index : Number | null = null
+
+ constructor(readonly webDisasterSummariesService: WebDisasterSummariesService) {
     if (!this.index) {
       this.index=-1
     }
-    */
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http' //https://angular.io/guide/http#setup-for-server-communication
-
+import { NgxPaginationModule } from 'ngx-pagination'
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
-import { DatasetGridComponent, DatasetViewerComponent } from './components';
-import { AgGridModule } from 'ag-grid-angular'
 import { FlexLayoutModule } from '@angular/flex-layout'
-import { FooterComponent } from './components'
-import { NgxPaginationModule } from 'ngx-pagination'
+
+import { DatasetGridComponent, DatasetViewerComponent, DatasetCardComponent, FooterComponent} from './components';
+import { AgGridModule } from 'ag-grid-angular'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DatasetGridComponent,
     DatasetViewerComponent,
+    DatasetCardComponent,
     FooterComponent,
   ],
   imports: [
